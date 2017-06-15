152eb4b73a0cb0fa7fa5e054e9c6b8db640e429f
赵博文 <nick.bw.zhao@outlook.com>
2017-06-15 17:30:29 +0800

    添加 redux

--- src/App.js
+++ src/App.js
@@ -2,107 +2,31 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  NavLink,
+  // Link,
+  Redirect,
+  // withRouter,
+  // MemoryRouter
 } from 'react-router-dom'
 
-import './App.css'
-import WordAdder from './ListOfWords'
-import CounterButton from './btn'
-import CustomTextInput from './Refs'
-import Calculator from './stateUp'
-import {NameForm} from './forms'
-import {NumberList} from './listItems'
-import LoginControl from './isLogoin'
-import Clock from './Clock'
-import Toggle from './Toggle'
-import Comment from './Comment'
-
-const Child = ({ match }) => (
-  <div>
-    <h3>ID: {match.params.id}</h3>
-  </div>
-)
-
-const BasicExample = () => (
-  <Router>
+const home = () => <div>home</div>
+const repos = () => <div>repos</div>
+const about = () => <div>about</div>
+const supportsHistory = 'pushState' in window.history
+const AuthExample = () => (
+  <Router forceRefresh={!supportsHistory}>
     <div>
-      <ul>
-        <li><Link to="/WordAdder">WordAdder</Link></li>
-        <li><Link to="/CounterButton">CounterButton</Link></li>
-        <li><Link to="/CustomTextInput">CustomTextInput</Link></li>
-        <li><Link to="/Calculator">Calculator</Link></li>
-        <li><Link to="/NameForm">NameForm</Link></li>
-        <li><Link to="/NumberList">NumberList</Link></li>
-        <li><Link to="/LoginControl">LoginControl</Link></li>
-        <li><Link to="/Clock">Clock</Link></li>
-        <li><Link to="/Toggle">Toggle</Link></li>
-      </ul>
-
-      <hr/>
-
-      <Route path="/:id" component={Child}/>
-
-      <hr/>
-
-      <Route exact path="/WordAdder" component={WordAdder}/>
-      <Route path="/CounterButton" component={CounterButton}/>
-      <Route path="/CustomTextInput" component={CustomTextInput}/>
-      <Route path="/Calculator" component={Calculator}/>
-      <Route path="/NameForm" component={NameForm}/>
-      <Route path="/NumberList" component={NumberList}/>
-      <Route path="/LoginControl" component={LoginControl}/>
-      <Route path="/Clock" component={Clock}/>
-      <Route path="/Toggle" component={Toggle}/>
-      <Route path="/Comment" component={Comment}/>
+      <li><NavLink to="/">home</NavLink></li>
+      <li><NavLink to="repos">repos</NavLink></li>
+      <li><NavLink to="about">about</NavLink></li>
+      <Route exact path="/" render={() => (
+        <Redirect to="/repos"/>
+      )}/>
+      <Route exact path="/" component={home}/>
+      <Route path="/repos" component={repos}/>
+      <Route path="/about" component={about}/>
     </div>
   </Router>
 )
 
-export default BasicExample
-
-
-// const Home = () => (
-//   <div>
-//     <h2>Home</h2>
-//   </div>
-// )
-//
-// const About = () => (
-//   <div>
-//     <h2>About</h2>
-//   </div>
-// )
-//
-// const Topic = ({ match }) => (
-//   <div>
-//     <h3>{match.params.topicId}</h3>
-//   </div>
-// )
-//
-// const Topics = ({ match }) => (
-//   <div>
-//     <h2>Topics</h2>
-//     <ul>
-//       <li>
-//         <Link to={`${match.url}/rendering`}>
-//           Rendering with React
-//         </Link>
-//       </li>
-//       <li>
-//         <Link to={`${match.url}/components`}>
-//           Components
-//         </Link>
-//       </li>
-//       <li>
-//         <Link to={`${match.url}/props-v-state`}>
-//           Props v. State
-//         </Link>
-//       </li>
-//     </ul>
-//
-//     <Route path={`${match.url}/:topicId`} component={Topic}/>
-//     <Route exact path={match.url} render={() => (
-//       <h3>Please select a topic.</h3>
-//     )}/>
-//   </div>
-// )
+export default AuthExample
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import ReactDOM, {render} from 'react-dom'
 import {render} from 'react-dom'
 // import App from './App'
-import AuthExample from './Ap'
+import AuthExample from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
